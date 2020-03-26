332d5e33f8b354e10172ce1d5d0d5d2bd616f53f
Madis Võrklaev <madisvorklaev@gmail.com>
2020-03-26 11:42:48 +0200

    packet recieved delay removed

--- NTP_clock.ino
+++ NTP_clock.ino
@@ -23,8 +23,10 @@ unsigned int localPort = 8888;       // local port to listen for UDP packets
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
-unsigned long previousMillis = 0;
-const long interval = 3600;
+unsigned long previousSendMillis = 0;
+unsigned long previousCheckMillis = 0;
+const long sendInterval = 3600;
+const long checkInterval = 1000;
 
 // A UDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
@@ -94,43 +96,42 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  if (currentMillis - previousSendMillis >= sendInterval) {
+    previousSendMillis = currentMillis;
     sendNTPpacket(timeServer); // send an NTP packet to a time server
-
-  // wait to see if a reply is available
-  delay(100);
-  if (Udp.parsePacket()) {
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, extract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-   // printMsg(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-    RTC.adjust(DateTime(epoch));
+  }
+  if (currentMillis - previousCheckMillis >= checkInterval) {
+    previousCheckMillis = currentMillis;  
+    if (Udp.parsePacket()) {
+      // We've received a packet, read the data from it
+      Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  
+      // the timestamp starts at byte 40 of the received packet and is four bytes,
+      // or two words, long. First, extract the two words:
+  
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      // combine the four bytes (two words) into a long integer
+      // this is NTP time (seconds since Jan 1 1900):
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      Serial.print("Seconds since Jan 1 1900 = ");
+      Serial.println(secsSince1900);
+     // printMsg(secsSince1900);
+  
+      // now convert NTP time into everyday time:
+      Serial.print("Unix time = ");
+      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      const unsigned long seventyYears = 2208988800UL;
+      // subtract seventy years:
+      unsigned long epoch = secsSince1900 - seventyYears;
+      // print Unix time:
+      Serial.println(epoch);
+  
+      RTC.adjust(DateTime(epoch));
   }}
 
     Serial.println();
     printRTCtime();
-    Serial.print(currentMillis - previousMillis);
     /*
     int hours = ((epoch  % 86400L) / 3600);
     int minutes = ((epoch  % 3600) / 60);
@@ -157,7 +158,7 @@ void loop() {
     Serial.println(epoch % 60); // print the second
   }
   */
-  // wait ten seconds before asking for the time again
+
   delay(1000);
   Ethernet.maintain();
 }
