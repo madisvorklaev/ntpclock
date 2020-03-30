a724c2c31cd0a53a321c7af26daaa40e42ce7ccd
Madis Võrklaev <madisvorklaev@gmail.com>
2020-03-31 00:04:16 +0300

    RTC SQW used as physical interrupt - second sync is perfect!

--- NTP_clock.ino
+++ NTP_clock.ino
@@ -23,15 +23,21 @@ unsigned int localPort = 8888;       // local port to listen for UDP packets
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
+const byte ledPin = 13;
+const byte interruptPin = 7;
+volatile byte state = LOW;
+int kkk=0;
+
 unsigned long previousSendMillis = 0;
 unsigned long previousCheckMillis = 0;
-//const long sendInterval = 900000;
-const long sendInterval = 10000;
+const long sendInterval = 900000;
+//const long sendInterval = 3000;
 const long checkInterval = 1000;
 
 int seconds;
 int lastSecond;
 bool flag;
+bool startupFlag = 1;
 
 // A UDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
@@ -39,7 +45,13 @@ EthernetUDP Udp;
 void setup() {
 
   Wire.begin();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x07);
+  Wire.write(0x10);  // Set Square Wave to 1 Hz
+  Wire.endTransmission();  
   RTC.begin();
+//  RTC.writeSqwPinMode(DS1307_SquareWave1Hz);
+//  RTC.squareWave(SQWAVE_1_HZ);
   lcd.begin();
   //lcd.backlight();
   if (! RTC.isrunning()) {
@@ -64,6 +76,10 @@ void setup() {
   lcd.print(ip);
   delay(2000);
   lcd.clear();
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
 
   /*
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -101,13 +117,13 @@ void setup() {
     
   }
   */
-  
+ /* 
 // initialize timer1 
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
-  OCR1A = 62500;            // compare match register 16MHz/256/1Hz (16000000Hz/256val/1Hz = 62500 = 1sek)
+  OCR1A = 6250;            // compare match register 16MHz/256/1Hz (16000000Hz/256val/1Hz = 62500 = 1sek)
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS12);    // 256 prescaler 
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
@@ -116,6 +132,7 @@ void setup() {
 
 ISR(TIMER1_COMPA_vect){    // timer compare interrupt service routine
   flag = 1;                // tick every second
+*/
 }
 
 void loop() {
@@ -142,14 +159,23 @@ void loop() {
       Serial.print("lowWord: ");
       Serial.println(lowWord);
 */
-
+/*
       // https://forum.arduino.cc/index.php?topic=526792.0
       // Combine the 4 timestamp bytes into one 32-bit number
       uint32_t NTPTime = (packetBuffer[40] << 24) | (packetBuffer[41] << 16) | (packetBuffer[42] << 8) | packetBuffer[43];     
       // Now get the fractional part
       uint32_t NTPmillis = (packetBuffer[44] << 24) | (packetBuffer[45] << 16) | (packetBuffer[46] << 8) | packetBuffer[47];
       int32_t fractionalPart = (int32_t)(((float)NTPmillis / UINT32_MAX) * 1000);
-   /*   Serial.print("NTPtime: ");
+
+   
+      // Increment the seconds as we are waiting for the next one
+      NTPTime++;  
+
+      // Burn off the remaining fractional part of the existing second
+      delay(1000 - fractionalPart);  
+*/
+   
+   /* Serial.print("NTPtime: ");
       Serial.println(NTPTime);
       Serial.print("NTPmillis: ");
       Serial.println(NTPmillis);
@@ -170,9 +196,9 @@ void loop() {
       RTC.adjust(DateTime(epoch));
   }}
 
-  if(flag == 1){
+  if(state = !state){
     printRTCtime();
-    flag = 0; }
+   }
 
     /*
     int hours = ((epoch  % 86400L) / 3600);
@@ -224,13 +250,14 @@ void sendNTPpacket(const char * address) {
 
 void printRTCtime(){
   DateTime now = RTC.now();
-
+  lcd.setCursor(0, 0);
+  lcd.print(millis() - previousSendMillis);
   lcd.setCursor(0, 1);
   print2digits(now.hour());
   lcd.print(':');
   print2digits(now.minute());
   lcd.print(':');
-  print2digits(now.second());
+  print2digits(now.second()+1);
   
   //unsigned long unix = now.unixtime();
   //Serial.print(now.year());
@@ -256,11 +283,15 @@ void printRTCtime(){
 }
 
 void print2digits(int number) {
+  if (number == 60) {
+    lcd.print("00");
+  }
+  else{
   if (number >= 0 && number < 10) {
     lcd.print('0');
   }
   lcd.print(number);
-}
+}}
 
 void checkEpoch(unsigned long ntpTime){
   DateTime now = RTC.now();
@@ -271,4 +302,11 @@ void checkEpoch(unsigned long ntpTime){
   lcd.setCursor(0, 1);
   lcd.print(rtcTime);
   lcd.println(" RTC");
+}
+
+void blink() {
+  state = !state;
+  Serial.println(state);
+  digitalWrite(ledPin, state);
+
 }
