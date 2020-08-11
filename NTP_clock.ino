9f669de87cb7341970cf3f83a82329b0b9d32af1
Madis Võrklaev <madisvorklaev@gmail.com>
2020-08-11 08:17:40 +0300

    Siimule saadetud versioon

--- NTP_clock.ino
+++ NTP_clock.ino
@@ -11,8 +11,15 @@ U8G2_T6963_256X64_F_8080 u8g2(U8G2_R0, 12, 1, 11, 2, 7, 3, 19, 4, /*enable/wr=*/
 
 byte mac[] = {0xA8, 0x61, 0x0A, 0xAE, 0x5A, 0x4B};
 //byte mac[] = {0xA8, 0x61, 0x0A, 0xAE, 0x5A, 0x3A};
+
+const char timeServer[] = "time.nist.gov";
+// IPAddress ip(192, 168, 1, 188); //disable for DHCP
+// IPAddress myDns(192, 168, 1, 1); //disable for DHCP
+// IPAddress gateway(192, 168, 1, 1); //disable for DHCP
+// IPAddress subnet(255, 255, 255, 0); //disable for DHCP
+
 unsigned int localPort = 8888;       // local port to listen for UDP packets
-const char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
+
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 EthernetUDP Udp; // A UDP instance to let us send and receive packets over UDP
@@ -25,27 +32,28 @@ volatile byte flag = LOW;
 volatile byte startup = HIGH;
 volatile byte noEth = LOW;
 
-uint8_t counter = 0;
-uint8_t sendInterval = 601;
+uint16_t counter = 0;
+uint16_t sendInterval = 601; //time in seconds between NTP requests
 uint8_t checkInterval = 1;
 
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 1000;
 
-const byte heartbeat = 16;
+const byte heartbeat = 16; //blinking led @D16
 
 void setup()
 {
   Ethernet.init(5);   // MKR ETH shield
-  Ethernet.begin(mac);
+  Ethernet.begin(mac); //DHCP
+//Ethernet.begin(mac, ip, myDns, gateway, subnet); //static IP (DNS, GW, SN are optional)
   Udp.begin(localPort);
   
   pinMode(heartbeat, OUTPUT);
   pinMode(20, OUTPUT);
   digitalWrite(20, HIGH);
   
-  Serial.begin(115200);
+ // Serial.begin(115200);
 
   rtc.begin(); // initialize RTC 24H format
   rtc.enableAlarm(rtc.MATCH_SS); //set interrupt mask to match seconds
@@ -70,7 +78,7 @@ void loop(){
   currentMillis = millis();
   if (currentMillis - startMillis >= period) {
     printRTCtime();
-    Serial.println("Millis took over!");
+ //   Serial.println("Millis took over!");
     startMillis = millis();
   }
   
@@ -87,18 +95,20 @@ void loop(){
   if (startup == HIGH && noEth == LOW) {
     sendNTPpacket(timeServer); // send an NTP packet to a time server
     startup = LOW;
-    Serial.println("First packet sent");
+  //  Serial.println("First packet sent");
   }
 
   if (counter >= sendInterval) {
     counter = sendInterval -1; // if NTP response hasn't arrived, send a new one in one second
     sendNTPpacket(timeServer); // send an NTP packet to a time server
-    Serial.println("NTP packet sent");
+ //   Serial.print("Counter value: ");
+ //   Serial.println(counter);
+ //   Serial.println("NTP packet sent");
       }
   
   if (Udp.parsePacket()) {
     counter = 0;
-    Serial.println("Packet recieved");
+ //   Serial.println("Packet recieved");
     Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
     // the timestamp starts at byte 40 of the received packet and is four bytes,
     // or two words, long. First, extract the two words:
@@ -112,9 +122,9 @@ void loop(){
     const unsigned long seventyYears = 2208988800UL;
     // subtract seventy years:
     unsigned long epoch = secsSince1900 - seventyYears;
-    Serial.println(epoch);
+    // Serial.println(epoch);
     rtc.setEpoch(epoch);
-    Serial.println("Time set on RTC");
+    // Serial.println("Time set on RTC");
 }}
 
 void sendNTPpacket(const char * address) { // send an NTP request to the time server at the given address
@@ -147,7 +157,7 @@ void tick(void)
      counter++;
      //Serial.print("Counter value: ");
      //Serial.println(counter);
-     digitalWrite(heartbeat, !digitalRead(heartbeat));                           // Toggle digital pin D16
+     digitalWrite(heartbeat, !digitalRead(heartbeat));                    // Toggle digital pin D16
      RTC->MODE2.INTFLAG.reg = RTC_MODE2_INTFLAG_ALARM0;                   // Reset interrupt flag     
      RTC->MODE2.Mode2Alarm[0].ALARM.bit.SECOND = (RTC->MODE2.Mode2Alarm[0].ALARM.bit.SECOND + 1) % 60;   // Increment the ALARM0 compare register
      while (RTC->MODE2.STATUS.bit.SYNCBUSY);                              // Wait for synchronization
